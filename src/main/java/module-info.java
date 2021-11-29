52b3b037541fb0a4494e4e6072d25fe2b8156bd7
Rodrigo <rvargas@fi.uba.ar>
2021-11-29 11:22:27 -0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
     exports edu.fiuba.algo3;
+    requires javafx.graphics;
+    exports edu.fiuba.algo3.modelo;
+    opens edu.fiuba.algo3.modelo;
 }
