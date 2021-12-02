4bc8c8c478b97b7d566137d0bd416e9fbfb3135e
LucasOshiro <71945088+LucasOshiro1@users.noreply.github.com>
2021-12-02 18:09:13 -0300

    Create modelo RespuestasDelEvento

---
+++ src/main/java/edu/fiuba/algo3/modelo/LadronAtrapado.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.modelo;
+
+public class LadronAtrapado implements RespuestasDelEvento {
+    String arresto = "Policia atrapó al ladron";
+
+    public String obtenerRespuesta() {
+        return arresto;
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/LadronEscapo.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.modelo;
+
+public class LadronEscapo implements RespuestasDelEvento {
+    String huida = "Ladrón escapó";
+
+    public String obtenerRespuesta() {
+        return huida;
+    }
+}
