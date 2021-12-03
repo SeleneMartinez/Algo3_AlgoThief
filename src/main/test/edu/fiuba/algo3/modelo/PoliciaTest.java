4f249612834fa474f72e433456230b5537dfea80
franciscoduc4 <fduca@fi.uba.ar>
2021-12-03 16:08:45 -0300

    agrego test policia

---
+++ src/main/test/edu/fiuba/algo3/modelo/PoliciaTest.java
@@ -0,0 +1,25 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PoliciaTest {
+    @Test
+    public void testInicializarPoliciaConNombre() {
+        Policia policia = new Policia("Roberto");
+        assertEquals("Roberto", policia.obtenerNombre());
+    }
+
+    @Test
+    public void testPoliciaRecienInicializadoNoTieneCasosResueltos() {
+        Policia policia = new Policia("Roberto");
+        assertEquals(0, policia.cantidadDeCasosResueltos());
+    }
+
+    @Test
+    public void testPoliciaNuevoEsNovato(){
+        RangoNovato rangoTest = new RangoNovato();
+        Policia policia = new Policia("Roberto");
+        assertEquals(rangoTest, policia.obtenerRangoPolicia());
+    }
+}
