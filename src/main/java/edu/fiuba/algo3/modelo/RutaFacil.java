1402935a64153e8f89745a36a4232595e29cfe35
federubachin <54431103+federubachin@users.noreply.github.com>
2021-12-03 05:40:46 -0300

    Update RutaFacil.java
    por las dudas revise alguien pero creo que esta bien esto

--- src/main/java/edu/fiuba/algo3/modelo/RutaFacil.java
+++ src/main/java/edu/fiuba/algo3/modelo/RutaFacil.java
@@ -6,8 +6,18 @@ import java.util.List;
 
 public class RutaFacil implements RutaDeEscape{
     List<Ciudad> ciudadesDeLaRuta = new ArrayList<Ciudad>();
+    List<Ciudad> ciudadesConectadas = new ArrayList<Ciudad>();
     public void agregarCiudadEnLaRuta(Ciudad ciudad) {
-        ciudadesDeLaRuta.add(ciudad);
+        {
+            ciudadesDeLaRuta.add(ciudad);
+            ciudadesConectadas.add(ciudad.devolverCiudadesConectadas());
+            ciudadesDeLaRuta.add(ciudadesConectadas[0]);
+            ciudadesDeLaRuta.add(ciudadesConectadas[1]);
+            ciudadesDeLaRuta.add(ciudadesConectadas[2]);
+
+
+
+        }
     }
     Iterator<Ciudad> iteradorCiudades= ciudadesDeLaRuta.iterator();
     private Ciudad proximaCiudad = iteradorCiudades.next();
