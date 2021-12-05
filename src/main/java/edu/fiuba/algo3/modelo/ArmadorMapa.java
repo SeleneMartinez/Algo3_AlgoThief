39d3b8566d676944b44efb4f59db483cde9d3c09
Rodrigo <rvargas@fi.uba.ar>
2021-12-05 19:53:22 -0300

    .

---
+++ src/main/java/edu/fiuba/algo3/modelo/ArmadorMapa.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArmadorMapa {
+    
+    List <Ciudad> rutaLadron = new ArrayList<Ciudad>();
+    List <Ciudad> paisesSinPista = new ArrayList<Ciudad>();
+}
