b097f13d89813ddd5cc1013d22017983ac833ff2
smartinez <semartinez@fi.uba.ar>
2021-12-05 14:34:46 -0300

    comente la prueba de policia

--- src/main/test/algothief/PoliciaTest.java
+++ src/main/test/algothief/PoliciaTest.java
@@ -1,4 +1,4 @@
-package algothief;
+/*package algothief;
 import edu.fiuba.algo3.modelo.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,3 +23,4 @@ public class PoliciaTest {
         assertEquals(rangoTest, policia.obtenerRangoPolicia());
     }
 }
+*/
--- src/main/test/algothief/UbicacionTest.java
+++ src/main/test/algothief/UbicacionTest.java
@@ -1,5 +1,16 @@
 package algothief;
-
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class UbicacionTest {
-    
+
+    @Test
+    public void obtieneDistanciaCorrecta(){
+        Ubicacion origen = new Ubicacion(200, 100);
+        Ubicacion destino = new Ubicacion(100, 50);
+
+        int distancia = origen.calcularDistancia(destino);
+
+        assertEquals(111,distancia);
+    }
 }
