0dbec551caaaf9c4ff5c3f1ef837d711512b0270
smartinez <semartinez@fi.uba.ar>
2021-12-05 21:03:37 -0300

    Arregle test de inicializador de ciudades

--- src/main/java/edu/fiuba/algo3/modelo/InicializadorCiudades.java
+++ src/main/java/edu/fiuba/algo3/modelo/InicializadorCiudades.java
@@ -10,19 +10,19 @@ public class InicializadorCiudades {
     public InicializadorCiudades() {}
 
     public List<Ciudad> iniciarCiudades() throws FileNotFoundException {
-        List<Ciudad> ciudades = new ArrayList<>();
-        JsonStreamParser parser = new JsonStreamParser(new FileReader("C:\\Users\\smartinez\\Documents\\Algoritmos\\Algo3_AlgoThief\\src\\main\\resources\\ciudades.json"));
-        JsonElement json =  parser.next();
+        List<Ciudad> ciudades = new ArrayList<Ciudad>();
+        JsonStreamParser parser = new JsonStreamParser(new FileReader("src/main/resources/ciudades.json"));
+        JsonElement json = parser.next();
         JsonArray jsonArray = json.getAsJsonArray();
         for (int i = 0; i < jsonArray.size(); i++) {
             String nombreCiudad = (jsonArray.get(i).getAsJsonObject().get("Ciudad")).getAsString();
             String pais = (jsonArray.get(i).getAsJsonObject().get("Pais")).getAsString();
-            int latitud = (jsonArray.get(i).getAsJsonObject().get("latitud")).getAsInt();
-            int altitud = (jsonArray.get(i).getAsJsonObject().get("altitud")).getAsInt();
+            int latitud = (jsonArray.get(i).getAsJsonObject().get("Latitud")).getAsInt();
+            int altitud = (jsonArray.get(i).getAsJsonObject().get("Altitud")).getAsInt();
 
-            Ubicacion ubicacion = new Ubicacion(altitud,latitud);
-            Ciudad nuevaCiudad = new Ciudad(nombreCiudad,ubicacion);
-            ciudades.add(nuevaCiudad);
+            Ubicacion ubicacion = new Ubicacion(altitud, latitud);
+            Ciudad ciudad = new Ciudad(nombreCiudad, ubicacion);
+            ciudades.add(ciudad);
         }
         return ciudades;
     }
--- src/main/test/algothief/InicializadorCiudadesTest.java
+++ src/main/test/algothief/InicializadorCiudadesTest.java
@@ -18,20 +18,7 @@ public class InicializadorCiudadesTest {
 
     @Test
     public void seInicianTodasLasCiudadesTest() throws FileNotFoundException {
-        List<Ciudad> ciudades = new ArrayList<Ciudad>();
-        JsonStreamParser parser = new JsonStreamParser(new FileReader("src/main/resources/ciudades.json"));
-        JsonElement json = parser.next();
-        JsonArray jsonArray = json.getAsJsonArray();
-        for (int i = 0; i < jsonArray.size(); i++) {
-            String nombreCiudad = (jsonArray.get(i).getAsJsonObject().get("Ciudad")).getAsString();
-            String pais = (jsonArray.get(i).getAsJsonObject().get("Pais")).getAsString();
-            int latitud = (jsonArray.get(i).getAsJsonObject().get("Latitud")).getAsInt();
-            int altitud = (jsonArray.get(i).getAsJsonObject().get("Altitud")).getAsInt();
-
-            Ubicacion ubicacion = new Ubicacion(altitud, latitud);
-            Ciudad ciudad = new Ciudad(nombreCiudad, ubicacion);
-            ciudades.add(ciudad);
-        }
+        List<Ciudad> ciudades = inicializadorCiudades.iniciarCiudades();
         assertEquals(30,ciudades.size());
     }
 }
