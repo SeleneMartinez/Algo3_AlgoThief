926f1eb1a8a616ea1d150c4189098ae51e33ce47
federubachin <54431103+federubachin@users.noreply.github.com>
2021-12-07 03:25:15 -0300

    Modifique rutaFacil y agrege las otras
    se repite mucho codigo

--- src/main/java/edu/fiuba/algo3/modelo/RutaDificil.java
+++ src/main/java/edu/fiuba/algo3/modelo/RutaDificil.java
@@ -5,27 +5,27 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-public class RutaFacil implements IRutaDeEscape{
+public class RutaDificil implements IRutaDeEscape{
     List<Ciudad> ciudadesDeLaRuta = new ArrayList<Ciudad>();
     List<Ciudad> ciudadesConectadas = new ArrayList<Ciudad>();
     Ciudad proximaCiudad;
     Ciudad ciudadActual;
     Random rand = new Random();
 
-    public RutaFacil (Ciudad ciudad){
+    public RutaDificil (Ciudad ciudad){
         this.ciudadActual = ciudad;
     }
 
-    public RutaFacil (){
+    public RutaDificil (){
         this.agregarCiudadesEnLaRuta();
     }
 
     public void agregarCiudadesEnLaRuta() {
 
-            int x = 1;
-            ciudadesDeLaRuta.add(ciudadActual);
-            while (x <= 4)
-            {
+        int x = 1;
+        ciudadesDeLaRuta.add(ciudadActual);
+        while (x <= 4)
+        {
             ciudadesConectadas.addAll(ciudadActual.devolverCiudadesConectadas());
             int int_random = rand.nextInt(ciudadesConectadas.size());
             ciudadesDeLaRuta.add(ciudadesConectadas.get(int_random));
@@ -43,3 +43,4 @@ public class RutaFacil implements IRutaDeEscape{
         }
     }
 }
+
