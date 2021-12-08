63499762707ec30f8e85b2cc6bf35204ec475e7e
LucasOshiro <71945088+LucasOshiro1@users.noreply.github.com>
2021-12-07 23:04:51 -0300

    Update CasoDeUso3Test.java

--- src/main/test/CasosDeUsoSem1/CasoDeUso3Test.java
+++ src/main/test/CasosDeUsoSem1/CasoDeUso3Test.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CasoDeUso3Test {
-    /**
     @Test
     public void detectiveViajeDeMontrealAMexico() {
         Integer altitud1 = 1200;
         Integer latitud1 = 600;
         Integer altitud2 = 900;
         Integer latitud2 = 400;
+        Integer horaEsperada = 7; //hay que ver lo de las horas en 0
         String nombreCiudad1 = "Montreal";
         String nombreCiudad2 = "Mexico";
         String nombrePolicia = "PoliNovato";
@@ -20,7 +20,10 @@ public class CasoDeUso3Test {
         Policia novato = new Policia(nombrePolicia);
         Ciudad montreal = new Ciudad(nombreCiudad1, new Ubicacion(altitud1, latitud1));
         Ciudad mexico = new Ciudad(nombreCiudad2, new Ubicacion(altitud2, latitud2));
+        
         novato.viajarDesdeHasta(montreal, mexico);
+       
+        assertEquals(horaEsperada, novato.obtenerHora());
      }
-     */
+     
 }
