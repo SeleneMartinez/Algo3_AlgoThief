1fcec46df1ce8dfc52bb190fa0a8f5c36f05ab71
smartinez <semartinez@fi.uba.ar>
2021-12-10 14:15:44 -0300

    reverted status

--- src/main/java/edu/fiuba/algo3/modelo/Ciudad.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ciudad.java
@@ -5,52 +5,90 @@ import java.util.List;
 
 public class Ciudad {
     String nombre;
-    Ubicacion ubicacion;
-    ConectorCiudades conector;
+    Ciudad ciudadAnterior;
     List<Ciudad> ciudadesConectadas = new ArrayList<Ciudad>();
+    Ubicacion ubicacion;
     List<IEdificio> edificiosEnCiudad = new ArrayList<IEdificio>(); // lista que contiene los 3 edificios en la cuidad
 
     public Ciudad(String nombre, Ubicacion ubicacion){
         this.nombre = nombre;
         this.ubicacion = ubicacion;
-        this.conector = conector;
     }
-
     public String darNombre(){
         return nombre;
     }
-
     public void agregarConexion(Ciudad ciudad) {
         ciudadesConectadas.add(ciudad);
     }
-
+    public void agregarCiudadAnterior(Ciudad ciudadAnterior) {
+        ciudadesConectadas.add(ciudadAnterior);
+        this.ciudadAnterior = ciudadAnterior;
+    }
     public void agregarEdificio(IEdificio edificio) {
         edificiosEnCiudad.add(edificio);
     }
-
-
-
-    public List<Ciudad> devolverCiudadesConectadasPrimeraVez() {
-        {
-            this.ciudadesConectadas = conector.agregarConexionesPrimeraVez(this);
-            return ciudadesConectadas;
-        }
+    public List<Ciudad> devolverCiudadesConectadas(){
+        return ciudadesConectadas;
     }
 
-    public List<Ciudad> devolverCiudadesConectadas(){
-        {
-            this.ciudadesConectadas = conector.agregarConexiones(this);
-            return ciudadesConectadas;
-        }
-
+    public Ciudad devolverCiudadAnterior() { 
+        return ciudadAnterior;
     }
 
     public List<IEdificio> devolverEdificios(){
         return edificiosEnCiudad;
     }
-
     public int obtenerDistanciaA(Ciudad destino){
         return this.ubicacion.calcularDistancia(destino.ubicacion);
     }
+
+    // String nombre;
+    // Ubicacion ubicacion;
+    // ConectorCiudades conector;
+    // List<Ciudad> ciudadesConectadas = new ArrayList<Ciudad>();
+    // List<IEdificio> edificiosEnCiudad = new ArrayList<IEdificio>(); // lista que contiene los 3 edificios en la cuidad
+
+    // public Ciudad(String nombre, Ubicacion ubicacion){
+    //     this.nombre = nombre;
+    //     this.ubicacion = ubicacion;
+    //      this.conector = conector;
+    // }
+
+    // public String darNombre(){
+    //     return nombre;
+    // }
+
+    // public void agregarConexion(Ciudad ciudad) {
+    //     ciudadesConectadas.add(ciudad);
+    // }
+
+    // public void agregarEdificio(IEdificio edificio) {
+    //     edificiosEnCiudad.add(edificio);
+    // }
+
+
+
+    // public List<Ciudad> devolverCiudadesConectadasPrimeraVez() {
+    //     {
+    //         this.ciudadesConectadas = conector.agregarConexionesPrimeraVez(this);
+    //         return ciudadesConectadas;
+    //     }
+    // }
+
+    // public List<Ciudad> devolverCiudadesConectadas(){
+    //     {
+    //         this.ciudadesConectadas = conector.agregarConexiones(this);
+    //         return ciudadesConectadas;
+    //     }
+
+    // }
+
+    // public List<IEdificio> devolverEdificios(){
+    //     return edificiosEnCiudad;
+    // }
+
+    // public int obtenerDistanciaA(Ciudad destino){
+    //     return this.ubicacion.calcularDistancia(destino.ubicacion);
+    // }
 
 }
--- src/main/test/algothief/CiudadTest.java
+++ src/main/test/algothief/CiudadTest.java
@@ -9,23 +9,23 @@ import java.util.List;
 public class CiudadTest {
 
     @Test
-    public void test01SeCreanCiudadesConCeroConexiones() {
+    public void conexionVacia() {
         List<Ciudad> ciudades = new ArrayList<Ciudad>();
         ciudades = crearCiudades();
-        for (Ciudad ciudad : ciudades)
+        for (Ciudad ciudad : ciudades){
             assertEquals(ciudad.devolverCiudadesConectadas().size(), 0);
+        }
     }
 
-
     @Test
-    public void test02ConexionesAgregadasCorrectamente() {
+    public void ciudadesConectadas() {
         List<Ciudad> ciudades = new ArrayList<Ciudad>();
         ciudades = crearCiudades();
-
-        ciudades.get(0).agregarConexion( ciudades.get(1) );//a la primera ciudad le conectamos 2 ciudades
+        ciudades.get(0).agregarConexion( ciudades.get(1) );
         ciudades.get(0).agregarConexion( ciudades.get(2) );
-        ciudades.get(1).agregarConexion( ciudades.get(4) );//a la segunda ciudad le conectamos 1 ciudades
-
+        ciudades.get(1).agregarConexion( ciudades.get(4) );
+        assertEquals(ciudades.get(0).devolverCiudadesConectadas().size(), 2);
+        assertEquals(ciudades.get(1).devolverCiudadesConectadas().size(), 1);
         assertEquals(ciudades.get(0).devolverCiudadesConectadas().get(0).darNombre(), "Montreal" );
         assertEquals(ciudades.get(0).devolverCiudadesConectadas().get(1).darNombre(), "Berlin" );
         assertEquals(ciudades.get(1).devolverCiudadesConectadas().get(0).darNombre(), "Moscu" );
@@ -51,6 +51,50 @@ public class CiudadTest {
         ciudades.add(moscu);
         return ciudades;
     }
-
-
 }
+//     @Test
+//     public void test01SeCreanCiudadesConCeroConexiones() {
+//         List<Ciudad> ciudades = new ArrayList<Ciudad>();
+//         ciudades = crearCiudades();
+//         for (Ciudad ciudad : ciudades)
+//             assertEquals(ciudad.devolverCiudadesConectadas().size(), 0);
+//     }
+
+
+//     @Test
+//     public void test02ConexionesAgregadasCorrectamente() {
+//         List<Ciudad> ciudades = new ArrayList<Ciudad>();
+//         ciudades = crearCiudades();
+
+//         ciudades.get(0).agregarConexion( ciudades.get(1) );//a la primera ciudad le conectamos 2 ciudades
+//         ciudades.get(0).agregarConexion( ciudades.get(2) );
+//         ciudades.get(1).agregarConexion( ciudades.get(4) );//a la segunda ciudad le conectamos 1 ciudades
+
+//         assertEquals(ciudades.get(0).devolverCiudadesConectadas().get(0).darNombre(), "Montreal" );
+//         assertEquals(ciudades.get(0).devolverCiudadesConectadas().get(1).darNombre(), "Berlin" );
+//         assertEquals(ciudades.get(1).devolverCiudadesConectadas().get(0).darNombre(), "Moscu" );
+//     }
+
+//     public List<Ciudad> crearCiudades() {
+//         List<Ciudad> ciudades = new ArrayList<Ciudad>();
+//         Ubicacion ubicacionArgentina = new Ubicacion(50,50);
+//         Ubicacion ubicacionMontreal = new Ubicacion(200,250);
+//         Ubicacion ubicacionBerlin = new Ubicacion(250,250);
+//         Ubicacion ubicacionSidney = new Ubicacion(300,350);
+//         Ubicacion ubicacionMoscu = new Ubicacion(350,350);
+
+//         Ciudad buenosAires = new Ciudad("Argentina", ubicacionArgentina);
+//         Ciudad montreal = new Ciudad("Montreal", ubicacionMontreal);
+//         Ciudad berlin = new Ciudad("Berlin", ubicacionBerlin);
+//         Ciudad sidney = new Ciudad("Sidney",ubicacionSidney);
+//         Ciudad moscu = new Ciudad("Moscu", ubicacionMoscu);
+//         ciudades.add(buenosAires);
+//         ciudades.add(montreal);
+//         ciudades.add(berlin);
+//         ciudades.add(sidney);
+//         ciudades.add(moscu);
+//         return ciudades;
+//     }
+
+
+// }
