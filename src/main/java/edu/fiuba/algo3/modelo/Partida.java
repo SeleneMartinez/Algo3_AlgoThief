46392a7e3e1a7b001edf1ea09e73d52d47484767
LucasOshiro <71945088+LucasOshiro1@users.noreply.github.com>
2021-12-22 01:50:06 -0300

    agrego metodos nuevos a Partida

--- src/main/java/edu/fiuba/algo3/modelo/Partida.java
+++ src/main/java/edu/fiuba/algo3/modelo/Partida.java
@@ -3,11 +3,14 @@ package edu.fiuba.algo3.modelo;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class Partida {
     Ladron ladron;
     Policia policia;
     List<Ciudad> ciudades;
+    HashMap<String, List<Pista>> pistasPorCiudad;
+    Ciudad ciudadActual;
     /**
     public Partida(){
 
@@ -20,17 +23,46 @@ public class Partida {
     */
     public Partida(String nombrePolicia, List<Ciudad> ciudades, Ladron ladron) {
         policia = new Policia(nombrePolicia);
-        this.policia = policia;
+        //this.policia = policia;
         this.ladron = ladron;
         this.ciudades = ciudades;
         IRango rango = policia.obtenerRangoPolicia();
-        HashMap<String, List<Pista>> pistasPorCiudad = rango.obtenerPistaPorRango();
+        //HashMap<String, List<Pista>> pistasPorCiudad = rango.obtenerPistaPorRango();
+        pistasPorCiudad = rango.obtenerPistaPorRango();
+        this.setPistasEnCiudad(pistasPorCiudad);
     }
 
     private void setPistasEnCiudad(HashMap<String, List<Pista>> pistas){
         for (Ciudad ciudad : this.ciudades){
-            List<Pista> pista = pistas.get(ciudad.nombre);
+            List<Pista> pista = pistas.get(ciudad.darNombre());
+            for (int i = 0; i < 3; i++) {
+                if (i == 0) {
+                    ciudad.agregarEdificio(new Banco(pista.get(i)));
+                } else if (i == 1) {
+                    ciudad.agregarEdificio(new Aeropuerto(pista.get(i)));
+                } else {
+                    ciudad.agregarEdificio(new Biblioteca(pista.get(i)));
+                }
+            }
         }
+        Random random = new Random();
+        ciudadActual = ciudades.get(random.nextInt(ciudades.size()));
     }
+    public String getDia() {
+        return policia.obtenerDia();
+    }
+
+    public int getHora() {
+        return policia.obtenerHora();
+    }
+
+    public Ciudad getCiudadActual() {
+        return ciudadActual;
+    }
+
+    public Policia getPolicia() {
+        return policia;
+    }
+
 
 }
