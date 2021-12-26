91d1bfe86c8b9c8caff94e176e31728d31519965
LucasOshiro <71945088+LucasOshiro1@users.noreply.github.com>
2021-12-26 19:22:17 -0300

    Update PantallaPistas.java

--- src/main/java/edu/fiuba/algo3/controlador/PantallaPistas.java
+++ src/main/java/edu/fiuba/algo3/controlador/PantallaPistas.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.controlador;
 
-
+import edu.fiuba.algo3.Vista.PantallaFinal;
 import edu.fiuba.algo3.modelo.Algothief;
 import edu.fiuba.algo3.modelo.Ciudad;
 import edu.fiuba.algo3.modelo.Partida;
@@ -9,7 +9,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 import java.util.List;
+
+
 
 public class PantallaPistas {
     @FXML
@@ -25,17 +35,69 @@ public class PantallaPistas {
 
     public void iniciarPantallaPistas(Algothief algothief) {
         juego = algothief;
-        this.modificarTiempo();
+        Partida partida = juego.getPartida();
+        Integer h = partida.getHora();
+        String h2 = Integer.toString(h);
+        tiempo.setText(partida.getDia() + " " + h2 + " hs");
         ciudad.setText(juego.getPartida().getCiudadActual().darNombre());
 
     }
 
     private void modificarTiempo() {
-        Integer h = juego.getPartida().getHora();
+        Partida partida = juego.getPartida();
+        if (partida.partidaTerminada()) {
+            this.pantallaFinal(event);
+        }
+        Integer h = partida.getHora();
         String h2 = Integer.toString(h);
-        tiempo.setText(juego.getPartida().getDia() + " " + h2 + " hs");
+        tiempo.setText(partida.getDia() + " " + h2 + " hs");
 
     }
+    
+        private void pantallaFinal(ActionEvent event) {
+        try {
+            Object eventSource = event.getSource();
+            Node sourceAsNode = (Node) eventSource;
+            Scene sceneActual = sourceAsNode.getScene();
+            Window window = sceneActual.getWindow();
+            Stage stage = (Stage) window;
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pantallaFinal.fxml"));
+            Parent root = loader.load();
+            PantallaFinal pantalla = loader.getController();
+            pantalla.iniciarPantallaFinal(juego);
+            sceneActual.setRoot(root);
+            stage.setScene(sceneActual);
+            stage.setMaximized(true);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void volverPantallaPrincipal(ActionEvent event) {
+        try {
+            Object eventSource = event.getSource();
+            Node sourceAsNode = (Node) eventSource;
+            Scene sceneActual = sourceAsNode.getScene();
+            Window window = sceneActual.getWindow();
+            Stage stage = (Stage) window;
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pantallaPrincipal.fxml"));
+            Parent root = loader.load();
+            PantallaPrincipalControlador pantalla = loader.getController();
+            pantalla.iniciarPantallaPrincipal(juego);
+            sceneActual.setRoot(root);
+            stage.setScene(sceneActual);
+            stage.setMaximized(true);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
 
     public void getPistaEconomica(ActionEvent event) {
         Partida partida = juego.getPartida();
